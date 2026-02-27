91a1dc977f5862a02bfde430d14fed00504041de
sergiox76 <115598273+sergiox76@users.noreply.github.com>
2026-02-27 01:50:18 -0500

    3

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   images: {
     domains: [],
   },
-}
 
-module.exports = nextConfig
+  experimental: {
+    turbo: false,
+  },
+};
+
+module.exports = nextConfig;
